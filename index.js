ca14920700e5e404c0b12d0af950bc743e75a5fe
LAGEISTE David <lageiste.david@gmail.com>
2025-01-23 15:29:44 +0100

    add default route

--- index.js
+++ index.js
@@ -40,5 +40,9 @@ if (req.body.validationCode) {
   }
 });
 
+app.get("/", (req, res, next) => {
+    res.json("Server is running");
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
